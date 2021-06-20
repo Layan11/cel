800136fec64ec161f56446931dc5738d26f1b74a
carol <carol@10.0.0.7>
2021-06-21 02:40:46 +0300

    el restrictions popup bttl3 bs mra w7di.
    +do5ot 5alsa + cancel screenings for date 5alsa(ma 3da lmsg)

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/restrictionPopupController.java
@@ -0,0 +1,17 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class restrictionPopupController implements Initializable{
+	public static int popped;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		popped = 1;
+	}
+
+}
